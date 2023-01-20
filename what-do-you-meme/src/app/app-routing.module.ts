e9ac9b9f045e4778654660c9461c3e69cd296c55
JiriSimonov <jirisimonov@gmail.com>
2023-01-21 02:06:28 +0300

    feat: add basic routing

--- what-do-you-meme/src/app/app-routing.module.ts
+++ what-do-you-meme/src/app/app-routing.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
-import {NotFoundComponent} from "./not-found/not-found.component";
-import {AuthComponent} from "./auth/auth.component";
-import {LobbiesComponent} from "./lobbies/lobbies.component";
-import {UserProfileComponent} from "./user-profile/user-profile.component";
+import {UserGuestGuard} from "./guards/user-guest.guard";
+import {UserAuthGuard} from "./guards/user-auth.guard";
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent },
-  { path: 'lobbies', component: LobbiesComponent },
-  { path: 'lobby', component: LobbiesComponent },
-  { path: 'profile',  component: UserProfileComponent},
-  { path: '', component: AppComponent},
-  { path: '**', component: NotFoundComponent }
+  {
+    path: 'user/auth',
+    loadChildren: () => import('./user-auth/user-auth.module')
+      .then(module => module.UserAuthModule),
+    canLoad: [UserGuestGuard],
+    canActivate: [UserGuestGuard],
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module')
+      .then(module => module.UserModule),
+    canLoad: [UserAuthGuard],
+    canActivate: [UserAuthGuard],
+  },
+  { path: 'game', loadChildren: () => import('./game/game.module').then(module => module.GameModule)},
+  { path: 'lobbies', loadChildren: () => import('./lobbies/lobbies.module').then(module => module.LobbiesModule) },
+  { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(module => module.NotFoundModule)},
+  { path: '**', redirectTo: 'not-found'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [UserGuestGuard, UserAuthGuard]
 })
 export class AppRoutingModule { }
