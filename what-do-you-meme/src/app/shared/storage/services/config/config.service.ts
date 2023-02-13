48224c0df1f13aa8bdba1cac4508305df8e16840
JiriSimonov <jirisimonov@gmail.com>
2023-02-13 22:09:17 +0300

    fix: services

--- what-do-you-meme/src/app/shared/storage/services/config/config.service.ts
+++ what-do-you-meme/src/app/shared/storage/services/config/config.service.ts
@@ -1,7 +1,7 @@
 import { LocalStorageService } from '../local-storage/local-storage.service';
 
 export class ConfigService {
-  static SERVER_URL = 'https://wdym-js-er-sd.onrender.com';
+  static SERVER_URL = 'http://localhost:3000';
   static get socketConfig() {
     return {
       url: this.SERVER_URL,
@@ -13,3 +13,4 @@ export class ConfigService {
     };
   }
 }
+  
